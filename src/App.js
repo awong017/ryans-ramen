57d71ded0d9c1992e89b51a7580f7a02b98e6312
Adam Wong <adamwthedev@gmail.com>
2020-05-19 16:11:55 -0700

    added background image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import Context from './context';
+import Styled from 'styled-components';
 import Home from './home';
+
+const App = Styled.div`
+`
 
 const app = () => {
   const renderRoutes = () => {
